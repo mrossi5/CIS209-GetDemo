54961962d862436c5a072edbf0c378ca3ab1148e
Mark Rossi <mrossi5@dtcc.edu>
2016-08-29 09:36:34 -0400

    added handler for button1

--- CIS209-getDemo/Form1.cs
+++ CIS209-getDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace CIS209_getDemo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
